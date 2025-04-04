724ce54fb32a918a2587824b21188a878b151187
lenovo_irg <sepidenaderi82@gmail.com>
2025-04-04 20:53:11 +0330

    increased model accuracy to 72% and lower loss

--- CNN_Model.py
+++ CNN_Model.py
@@ -4,15 +4,35 @@ import tensorflow as tf
 
 train_dataset = 'Faceshape_dataset'
 
-face_shapes = ['heart', 'oval', 'oblong', 'square', 'round']
-
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    validation_split=0.2
+)
 
 train_generator = datagen.flow_from_directory(
     train_dataset,
     target_size=(128, 128),
     batch_size=32,
-    class_mode='categorical')
+    class_mode='categorical',
+    subset='training',
+    shuffle=True
+)
+
+val_generator = datagen.flow_from_directory(
+    train_dataset,
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation',
+    shuffle=True
+)
 
 model = Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
@@ -29,13 +49,15 @@ model = Sequential([
 
     layers.Flatten(),
 
-    layers.Dense(128, activation='relu'),
+    layers.Dense(256, activation='relu'),
     layers.Dropout(0.5),
     layers.Dense(5, activation='softmax')
 ])
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
-history = model.fit(train_generator, epochs=20)
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(train_generator, validation_data=val_generator, epochs=50)
 
 model.save('face_shape_model.keras')
